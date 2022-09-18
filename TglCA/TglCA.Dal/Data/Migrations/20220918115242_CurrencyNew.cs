2203ae82f377b6f98cde0ee030d4efd710025da7
maxKutvitzky <baznalt@gmail.com>
2022-09-18 15:30:26 +0300

    Small fixes

---
+++ TglCA/TglCA.Dal/Data/Migrations/20220918115242_CurrencyNew.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TglCA.Dal.Data.Migrations
+{
+    public partial class CurrencyNew : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
